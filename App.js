4f686215c755b851290140b1d79cf9932bbae334
Jitendra Mahatpure <jmahatpure01@gmail.com>
2019-12-03 01:04:34 +0530

    night1

--- App.js
+++ App.js
@@ -9,203 +9,195 @@ import {
   Button
 } from 'react-native';
 
+class Move {
+  row;
+  col;
+}
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      text1: "",
-      text2: "",
-      text3: "",
-      text4: "",
-      text5: "",
-      text6: "",
-      text7: "",
-      text8: "",
-      text9: "",
+      board: [["", "", ""], ["", "", ""], ["", "", ""]],
+      player: 'X',
+      ai: 'O',
+      msg: "Good Luck Player"
     }
+  }
+
+  isMovesLeft() {
+    for (let i = 0; i<3; i++) { 
+      for (let j = 0; j<3; j++) {
+        if (this.state.board[i][j]=="") {
+          return true; 
+        }
+      }
+    }
+    return false;
+  }
+
+  evaluate(board=null) {
+    if(board == null) {
+      board = this.state.board;
+    }
+    
+    for(let row=0; row < 3; row++) {
+      if(board[row][0] == board[row][1] && board[row][1] == board[row][2]) {
+        if(board[row][0] == this.state.ai) {
+          return +10;
+        } else if(board[row][0] == this.state.player) {
+          return -10;
+        }
+      }
+    }
+
+    for(let col=0; col < 3; col++) {
+      if(board[0][col] == board[1][col] && board[1][col] == board[2][col]) {
+        if(board[0][col] == this.state.ai) {
+          return +10;
+        } else if(board[0][col] == this.state.player) {
+          return -10;
+        }
+      }
+    }
+
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+      if(board[0][0] == this.state.ai) {
+        return +10;
+      } else if(board[0][0] == this.state.player) {
+        return -10;
+      }
+    }
+
+    if(board[2][0] == board[1][1] && board[1][1] == board[0][2]) {
+      if(board[0][2] == this.state.ai) {
+        return +10;
+      } else if(board[0][2] == this.state.player) {
+        return -10;
+      }
+    }
+
+    return 0;
+  }
+
+  minimax(board=null, depth, isMax) {
+    if(board == null){
+      board = this.state.board;
+    }
+    
+    let score = this.evaluate(board);
+
+    if(score == -10 || score == 10) {
+      return score;
+    }
+
+    if(this.isMovesLeft() == false) {
+      return 0;
+    }
+
+    if(isMax) {
+      let best = -1000;
+
+      for(let row = 0; row < 3; row++) {
+        for(let col = 0; col < 3; col++) {
+          if(board[row][col] == "") {
+            board[row][col] = this.state.ai;
+
+            best = Math.max(best, this.minimax(board, depth+1, !isMax));
+
+            board[row][col] = "";
+          }
+        }
+      }
+      return best;
+    } else {
+      let best = 1000;
+
+      for(let row = 0; row < 3; row++) {
+        for(let col = 0; col < 3; col++) {
+          if(board[row][col] == "") {
+            board[row][col] = this.state.player;
+
+            best = Math.max(best, this.minimax(board, depth+1, !isMax));
+
+            board[row][col] = "";
+          }
+        }
+      }
+      return best;
+    }
+  }
+
+  findTheBestMove() {
+    let bestVal = -1000;
+    let bestMove = new Move();
+    bestMove.row = -1;
+    bestMove.col = -1;
+    let board = this.state.board;
+
+    for(let row = 0; row < 3; row++) {
+      for(let col = 0; col < 3; col++) {
+        if(board[row][col] == "") {
+          board[row][col] = this.state.ai;
+
+          let moveVal = this.minimax(board, 0, false);
+
+          board[row][col] = "";
+
+          if(moveVal > bestVal) {
+            bestMove.row = row;
+            bestMove.col = col;
+            bestVal = moveVal;
+          }
+        }
+      }
+    }
+
+    return bestMove;
   }
 
   input(btn){
     console.log(btn)
+    let board = this.state.board;
     switch(btn) {
-      case 1 : this.setState({text1: 'X'});break;
-      case 2 : this.setState({text2: 'X'});break;
-      case 3 : this.setState({text3: 'X'});break;
-      case 4 : this.setState({text4: 'X'});break;
-      case 5 : this.setState({text5: 'X'});break;
-      case 6 : this.setState({text6: 'X'});break;
-      case 7 : this.setState({text7: 'X'});break;
-      case 8 : this.setState({text8: 'X'});break;
-      case 9 : this.setState({text9: 'X'});break;
+      case 1 : board[0][0] = this.state.player;
+      this.setState({board: board});break;
+      case 2 : board[0][1] = this.state.player;
+      this.setState({board: board});break;
+      case 3 : board[0][2] = this.state.player;
+      this.setState({board: board});break;
+      case 4 : board[1][0] = this.state.player;
+      this.setState({board: board});break;
+      case 5 : board[1][1] = this.state.player;
+      this.setState({board: board});break;
+      case 6 : board[1][2] = this.state.player;
+      this.setState({board: board});break;
+      case 7 : board[2][0] = this.state.player;
+      this.setState({board: board});break;
+      case 8 : board[2][1] = this.state.player;
+      this.setState({board: board});break;
+      case 9 : board[2][2] = this.state.player;
+      this.setState({board: board});break;
     }
-    this.checkB();
-    this.AInput(btn)
-  }
-
-  checkB() {
-
-  }
-
-  AInput(){
-    let board = this.state;
-    if(board.text1 == board.text2) {
-      if(board.text1 != board.text3) {
-        this.setState({text3: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text1 == board.text4) {
-      if(board.text1 != board.text7) {
-        this.setState({text7: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text1 == board.text5) {
-      if(board.text1 != board.text9) {
-        this.setState({text9: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text1 == board.text3) {
-      if(board.text1 != board.text2) {
-        this.setState({text2: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text1 == board.text7) {
-      if(board.text1 != board.text4) {
-        this.setState({text4: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text1 == board.text9) {
-      if(board.text1 != board.text5) {
-        this.setState({text5: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text2 == board.text3) {
-      if(board.text2 != board.text1) {
-        this.setState({text1: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text2 == board.text5) {
-      if(board.text2 != board.text8) {
-        this.setState({text8: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text2 == board.text8) {
-      if(board.text2 != board.text5) {
-        this.setState({text5: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text3 == board.text5) {
-      if(board.text3 != board.text7) {
-        this.setState({text7: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text3 == board.text6) {
-      if(board.text3 != board.text9) {
-        this.setState({text9: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text3 == board.text7) {
-      if(board.text3 != board.text5) {
-        this.setState({text5: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text4 == board.text5) {
-      if(board.text4 != board.text6) {
-        this.setState({text6: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text4 == board.text7) {
-      if(board.text4 != board.text1) {
-        this.setState({text1: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text5 == board.text6) {
-      if(board.text5 != board.text4) {
-        this.setState({text4: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text5 == board.text8) {
-      if(board.text5 != board.text2) {
-        this.setState({text2: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text5 == board.text3) {
-      if(board.text5 != board.text7) {
-        this.setState({text7: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text5 == board.text7) {
-      if(board.text5 != board.text3) {
-        this.setState({text3: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text5 == board.text9) {
-      if(board.text5 != board.text1) {
-        this.setState({text1: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text6 == board.text9) {
-      if(board.text6 != board.text3) {
-        this.setState({text3: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text6 == board.text4) {
-      if(board.text6 != board.text5) {
-        this.setState({text5: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text7 == board.text8) {
-      if(board.text7 != board.text9) {
-        this.setState({text9: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text7 == board.text9) {
-      if(board.text7 != board.text8) {
-        this.setState({text8: 'O'})
-      } else {
-        //user win
-      }
-    } else if(board.text8 == board.text9) {
-      if(board.text8 != board.text7) {
-        this.setState({text7: 'O'})
-      } else {
-        //user win
-      }
+    let move = this.findTheBestMove();
+    board[move.row][move.col] = this.state.ai;
+    this.setState({board: board});
+    let result = this.evaluate();
+    if(result == 10) {
+      this.setState({msg: "AI Won"})
+    } else if(result == -10) {
+      this.setState({msg: "Great! Player Won"})
+    } else if(result == 0 && this.isMovesLeft() == false) {
+      this.setState({msg: "A RARE Tie"})
     }
   }
 
   reset() {
     this.setState({
-      text1: "",
-      text2: "",
-      text3: "",
-      text4: "",
-      text5: "",
-      text6: "",
-      text7: "",
-      text8: "",
-      text9: "",
+      board: [["", "", ""], ["", "", ""], ["", "", ""]],
+      player: 'X',
+      ai: 'O',
+      msg: "Good Luck Player!"
     })
   }
 
@@ -213,25 +205,28 @@ class App extends Component {
     return (
       <View style={styles.body}>
         <Text style={styles.heading}>TicTacToe</Text>
+        <Text>{this.state.msg}</Text>
         <View style={styles.outerContainer}>
           <View style={styles.row}>
-            <Text style={styles.inputBox} onPress={()=>this.input(1)}>{this.state.text1}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(2)}>{this.state.text2}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(3)}>{this.state.text3}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(1)}>{this.state.board[0][0]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(2)}>{this.state.board[0][1]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(3)}>{this.state.board[0][2]}</Text>
           </View>
           <View style={styles.row}>
-            <Text style={styles.inputBox} onPress={()=>this.input(4)}>{this.state.text4}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(5)}>{this.state.text5}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(6)}>{this.state.text6}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(4)}>{this.state.board[1][0]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(5)}>{this.state.board[1][1]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(6)}>{this.state.board[1][2]}</Text>
           </View>
           <View style={styles.row}>
-            <Text style={styles.inputBox} onPress={()=>this.input(7)}>{this.state.text7}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(8)}>{this.state.text8}</Text>
-            <Text style={styles.inputBox} onPress={()=>this.input(9)}>{this.state.text9}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(7)}>{this.state.board[2][0]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(8)}>{this.state.board[2][1]}</Text>
+            <Text style={styles.inputBox} onPress={()=>this.input(9)}>{this.state.board[2][2]}</Text>
           </View>
         </View>
         <View style={styles.controls}>
           <Text>Game</Text>
+          <Text>Player: {this.state.player}</Text>
+          <Text>AI: {this.state.ai}</Text>
           <Button title="Reset" onPress={()=>this.reset()}/>
         </View>
       </View>
